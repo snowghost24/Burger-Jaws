9a6e66b74d242befaeaa56a62c5e73280d54487f
Snowghost24 <Snowghost@Joses-MacBook-Air.local>
2017-10-06 14:55:13 -0400

    added db connection

--- jawsdb.js
+++ jawsdb.js
@@ -1,18 +1,18 @@
 
-
-// var connection;
-// if (process.env.JAWSDB_URL){
-//    connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//    connection = mysql.createConnection({
-//       port: 3306,
-//       host: "localhost",
-//       user: "root",
-//       password: "",
-//       database: "day_planner_db "
-//     });
-// }
-// connection.connect();
+var mysql = require('mysql');
+var connection;
+if (process.env.JAWSDB_URL){
+   connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+   connection = mysql.createConnection({
+     
+      host: 'localhost',
+      user: 'root',
+      password: "",
+      database: "day_planner_db "
+    });
+}
+connection.connect();
 // connection.connect(function(err) {
 //    if (err) {
 //      console.error('error connecting: ' + err.stack);
@@ -23,18 +23,18 @@
 // // connection.end();
 // module.exports = connection;var mysql = require('mysql');
 
-var mysql = require('mysql');
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+// var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 
 
 
-connection.connect(function(err) {
-   if (err) {
-     console.error('error connecting: ' + err.stack);
-     return;
-   }
-   console.log('connected as id ' + connection.threadId);
- });
+// connection.connect(function(err) {
+//    if (err) {
+//      console.error('error connecting: ' + err.stack);
+//      return;
+//    }
+//    console.log('connected as id ' + connection.threadId);
+//  });
 
-connection.end();
+// connection.end();
